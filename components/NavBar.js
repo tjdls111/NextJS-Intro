62d9529f8cb4866e01c64bbf77cdccf489235f82
Seoin <68271159+tjdls111@users.noreply.github.com>
2022-02-27 19:46:01 +0900

    styles JSX

--- components/NavBar.js
+++ components/NavBar.js
@@ -8,24 +8,22 @@ export default function NavBar() {
   return (
     <nav className={styles.nav}>
       <Link href="/">
-        <a
-          className={`${styles.link} ${
-            router.pathname === "/" ? styles.active : ""
-          }`}
-        >
-          Home
-        </a>
+        <a className={router.pathname === "/" ? "active" : ""}>Home</a>
       </Link>
       <Link href="/about">
-        <a
-          className={[
-            styles.link,
-            router.pathname === "/about" ? styles.active : "",
-          ].join(" ")}
-        >
-          About
-        </a>
+        <a className={router.pathname === "/about" ? "active" : ""}>About</a>
       </Link>
+      <style jsx>{`
+        nav {
+          background-color: pink;
+        }
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: yellow;
+        }
+      `}</style>
     </nav>
   );
 }
